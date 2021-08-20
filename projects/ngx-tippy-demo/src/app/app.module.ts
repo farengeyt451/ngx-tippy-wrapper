f2946b210e558bb405dc8a36657056f66989cb70
Alexander Kislov <oleksandr_kyslov@epam.com>
2021-08-20 17:53:14 +0300

    Try setup tooltip

--- projects/ngx-tippy-demo/src/app/app.module.ts
+++ projects/ngx-tippy-demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxTippyModule } from 'ngx-tippy-wrapper';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxTippyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
