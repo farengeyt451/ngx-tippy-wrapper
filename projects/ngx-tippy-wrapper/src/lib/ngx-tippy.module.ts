b412ca751add25536f3600863d20b4a214227809
Alexander Kislov <strannik11.92@gmail.com>
2022-10-20 23:45:24 +0300

    Refactor tokens providing

--- projects/ngx-tippy-wrapper/src/lib/ngx-tippy.module.ts
+++ projects/ngx-tippy-wrapper/src/lib/ngx-tippy.module.ts
@@ -1,23 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgxTippyGroupComponent, NgxTippySingletonComponent } from './components';
-import { messagesDict, tippyFakeInstance } from './fixtures';
 import { NgxTippyDirective } from './ngx-tippy.directive';
-import { NGX_TIPPY_MESSAGES, TIPPY_FAKE_INSTANCE } from './ngx-tippy.tokens';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [NgxTippyDirective, NgxTippyGroupComponent, NgxTippySingletonComponent],
   exports: [NgxTippyDirective, NgxTippyGroupComponent, NgxTippySingletonComponent],
-  providers: [
-    {
-      provide: TIPPY_FAKE_INSTANCE,
-      useValue: tippyFakeInstance,
-    },
-    {
-      provide: NGX_TIPPY_MESSAGES,
-      useValue: messagesDict,
-    },
-  ],
 })
 export class NgxTippyModule {}
