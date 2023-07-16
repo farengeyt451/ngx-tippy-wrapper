b0e813cf65cbf35e8e468b8727d5db49de0b9487
Alexander Kislov <strannik11.92@gmail.com>
2023-07-16 21:42:36 +0300

    demo/2.0: Finish header

--- projects/ngx-tippy-demo/src/app/app.component.ts
+++ projects/ngx-tippy-demo/src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { WINDOW } from '@ng-web-apis/common';
+import { DestroyService, SchemeService } from '@services';
+import { takeUntil } from 'rxjs';
 import { Schemes } from '../interfaces/schemes.enum';
-import { SchemeService } from '../services/scheme-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DestroyService],
 })
 export class AppComponent {
   protected Schemes = Schemes;
+  private readonly bodyClassList!: DOMTokenList;
 
-  constructor(protected readonly schemeService: SchemeService) {}
+  constructor(
+    @Inject(WINDOW) readonly window: Window,
+    protected readonly schemeService: SchemeService,
+    private destroy$: DestroyService
+  ) {
+    this.bodyClassList = this.window.document.body.classList;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.listenForSchemeChange();
+  }
+
+  private listenForSchemeChange() {
+    this.schemeService.scheme$.pipe(takeUntil(this.destroy$)).subscribe(scheme => {
+      this.toggleBodyClass(scheme as Schemes);
+    });
+  }
+
+  private toggleBodyClass(scheme: Schemes) {
+    this.bodyClassList.remove(scheme === Schemes.Dark ? Schemes.Light : Schemes.Dark);
+    this.bodyClassList.add(scheme);
+  }
 }
