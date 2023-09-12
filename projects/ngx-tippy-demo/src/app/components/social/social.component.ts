b10d233ffed0ea5b75a91089b621d9ddce9b4030
Alexander Kislov <strannik11.92@gmail.com>
2023-09-12 20:04:12 +0300

    Upd support block

--- projects/ngx-tippy-demo/src/app/components/social/social.component.ts
+++ projects/ngx-tippy-demo/src/app/components/social/social.component.ts
@@ -9,12 +9,19 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 export class SocialComponent implements OnInit {
   protected readonly socials = [
     {
+      key: 'heart',
+      link: null,
+      routerLink: '/sponsor',
+    },
+    {
       key: 'linkedin',
       link: 'https://www.linkedin.com/in/alexander-kislov',
+      routerLink: null,
     },
     {
       key: 'github',
       link: 'https://github.com/farengeyt451',
+      routerLink: null,
     },
   ];
 
